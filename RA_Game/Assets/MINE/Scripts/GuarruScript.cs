de6d6b0f30554a2682ccb8dc695559006574ea30
Aitorlb7 <45202115+Aitorlb7@users.noreply.github.com>
2021-05-27 18:54:03 +0200

    Unlock / Lock button

--- RA_Game/Assets/MINE/Scripts/GuarruScript.cs
+++ RA_Game/Assets/MINE/Scripts/GuarruScript.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 
 public class GuarruScript : MonoBehaviour
 {
     public AnchorInputListenerBehaviour anchorbehaviour;
     public GameObject Floor;
+    public Text Unlock;
+    public bool unlockTerrain;
     void Start()
     {
        anchorbehaviour = gameObject.GetComponent<AnchorInputListenerBehaviour>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Floor.gameObject.GetComponent<MeshRenderer>().enabled == true)
-        {
-            anchorbehaviour.enabled = false;
-        }
-        else
+        if(unlockTerrain)
         {
             anchorbehaviour.enabled = true;
+            Unlock.text = "Lock";
         }
+        else if (Floor.gameObject.GetComponent<MeshRenderer>().enabled == true)
+        {
+            anchorbehaviour.enabled = false;
+            if (!unlockTerrain)
+            {
+                Unlock.text = "Unlock";
+            }
+        }
+    }
+
+    public void SwapUnlockTerrain()
+    {
+        unlockTerrain = !unlockTerrain;
     }
 }
